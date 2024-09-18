b5cea1a507bf5e63ad48e527414dfa963ec3555d
Reda Mhmd <redamhmd65@gmail.com>
2024-09-18 07:32:25 +0300

    edit structure

---
+++ cpp_module/primerPlus/3-oop/oop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class test
+{
+private:
+    /* data */
+
+public:
+    test(/* args */);
+    ~test();
+};
+
+test::test(/* args */)
+{
+}
+
+test::~test()
+{
+}
+
+
+/// @brief 
+/// @return 
+int main()
+{
+    
+    return 0;
+}
